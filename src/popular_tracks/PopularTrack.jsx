d67dab6db47a56efc532ee93a9c7a8863a191e91
dianorlova <dianaorlova0101@gmail.com>
2022-12-22 02:24:38 +0300

    Удалены вспомогательные комментарии старого кода. Разбиты css-файлы на модули

--- src/popular_tracks/PopularTrack.jsx
+++ src/popular_tracks/PopularTrack.jsx
@@ -8,7 +8,7 @@ import './popular_tracks.css';
 export default function PopularTrack({ track }) {
     return (
         <div className='track'>
-            <img className='track__img'></img>
+            <img className='track__img' alt=''></img>
             <div className='track__info'>
                 <h3 className='track__name'>
                     <a className='track__name__link track__name base_link' href={track.url}>{track.name}</a>
@@ -20,29 +20,3 @@ export default function PopularTrack({ track }) {
         </div>
     ) 
 }
-
-/*
-            const track_div = createHTMLElement('div', 'track');+
-            const track__img = createHTMLElement('img', 'track__img');+
-            const track__info = createHTMLElement('div', 'track__info');+
-            const track__name_h3 = createHTMLElement('h3', 'track__name');+
-            const track__name__link = createHTMLElement('a', 'track__name__link track__name base_link');+
-            track__name__link.href = track.url;+
-             
-            const track__nameArtist = createHTMLElement('p', 'track__nameArtist');+
-            const track__nameArtist__link = createHTMLElement('a', 'track__nameArtist__link track__nameArtist  base_link');+
-            track__nameArtist__link.href = track.artist.url;+
-
-            track__name__link.append(track.name);+
-            track__name_h3.append(track__name__link);+
-            track__info.append(track__name_h3);+
-            track__nameArtist__link.append(track.artist.name);+
-            track__nameArtist.append(track__nameArtist__link);+
-            track__info.append(track__nameArtist);+
-             
-            
-             track_div.append(track__img);+
-             track_div.append(track__info);+
-
-            popularTracks.append(track_div);
-*/
--- src/search_page/SearchedTrack.jsx
+++ src/search_page/SearchedTrack.jsx
@@ -12,28 +12,7 @@ export default function SearchedTrack({ track }) {
             <div className='play__track__search__item'></div>
             <div className='img__track__search__item'></div>
             <a className='track__name__link track__name base_link' href={track.url}>{track.name}</a>
-            <a className='track__nameArtist__link track__nameArtist base_link'>{track.artist}</a>
+            <a className='track__nameArtist__link track__nameArtist base_link' href='/'>{track.artist}</a>
         </div>
     )
 }
-/*
-                           const track__search__item = createHTMLElement('div', 'track__search__item');+
-                            const play__track__search__item = createHTMLElement('div', 'play__track__search__item');+
-
-                            const track__image = createHTMLElement('div', 'img__track__search__item');+
-
-                            const track__name__link = createHTMLElement('a', 'track__name__link track__name base_link');+
-                            track__name__link.href = track.url; +
-                            track__name__link.append(track.name);+
-
-                            const artist__name = createHTMLElement('a', 'track__nameArtist__link track__nameArtist base_link');+
-
-                            artist__name.append(track.artist);+
-                            
-                            track__search__item.append(play__track__search__item);+
-                            track__search__item.append(track__image);+
-                            track__search__item.append(track__name__link);+
-                            track__search__item.append(artist__name);
-
-                            tracks.append(track__search__item);+
-*/
